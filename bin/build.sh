50afe91425bdf41fa6eb7014436ca59484a4732d
Pauan <pcxunlimited@gmail.com>
2015-06-23 09:19:46 -1000

    Minor tweaks

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 # Cleanup old build dir
-rm -r build/gsap
+rm --recursive --force build/gsap
 
 cp node_modules/gsap/src/minified/TweenLite.min.js build/lib/gsap/TweenLite.min.js
 cp node_modules/gsap/src/minified/plugins/CSSPlugin.min.js build/lib/gsap/CSSPlugin.min.js
@@ -9,7 +9,7 @@ cp node_modules/gsap/src/minified/plugins/ScrollToPlugin.min.js build/lib/gsap/S
 
 cp node_modules/babel-core/browser-polyfill.min.js build/lib/browser-polyfill.min.js
 
-mkdir build/js -p
+mkdir build/js --parents
 
 browserify --transform babelify src/server.js --outfile build/js/server.js
 #browserify --transform babelify src/panel.js --outfile build/js/panel.js
