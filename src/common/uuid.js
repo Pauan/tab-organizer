bba5fc57a9b474ae33650334218d044dc58dcfb2
Pauan <pcxunlimited@gmail.com>
2015-07-25 00:26:32 -1000

    Removing sync

--- src/common/uuid.js
+++ src/common/uuid.js
@@ -1,2 +1,1 @@
-export const uuid_port_sync = "f4fb4e4b-6818-49da-bd82-5a2ee01ad589";
-export const uuid_port_tab  = "9a58f922-37ea-4af4-8718-06179f442957";
+export const uuid_port_tab = "9a58f922-37ea-4af4-8718-06179f442957";
--- src/options.js
+++ src/options.js
@@ -1,8 +1,5 @@
-import { init as init_sync } from "./client/sync";
 import { run_async } from "./util/async";
 
 run_async(function* () {
-  const db = yield init_sync;
-
   console["debug"]("options: initialized");
 });
