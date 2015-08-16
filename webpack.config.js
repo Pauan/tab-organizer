1d1c2f34125f9d0214f35bf5f051d5a839d47947
Pauan <pcxunlimited@gmail.com>
2015-08-16 04:38:56 -1000

    Only use certain transformers for Babel

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,27 @@ module.exports = {
         query: {
           //cacheDirectory: true,
           externalHelpers: true,
-          optional: [
+          nonStandard: false,
+          whitelist: [
+            "es6.arrowFunctions",
+            //"es6.blockScoping",
+            "es6.classes",
+            //"es6.constants",
+            "es6.destructuring",
+            //"es6.forOf",
+            "es6.modules",
+            "es6.parameters",
+            "es6.properties.computed",
+            "es6.properties.shorthand",
+            "es6.spread",
+            "es6.tailCall",
+            //"es6.templateLiterals",
+            //"es6.regex.unicode",
+            //"es6.regex.sticky",
+
+            //"regenerator",
+            "strict",
+
             "validation.undeclaredVariableCheck",
             //"runtime",
             //"minification.deadCodeElimination",
