f071c19ab55fb15258a6e763b1713943cb3f3c00
Pauan <pcxunlimited@gmail.com>
2015-08-31 11:57:31 -1000

    Fixing up json.js

--- src/util/json.js
+++ src/util/json.js
@@ -1,40 +1,39 @@
-import { map, each, entries } from "./iterator";
-import { List } from "./immutable/list";
-import { Record } from "./immutable/record";
-import { fail } from "./assert";
+const is_object = (x) =>
+  Object["getPrototypeOf"](x) === Object["prototype"];
 
 
-export const to_json = (x) => {
+export const is_json = (x) => {
   if (x === null ||
       x === true ||
       x === false ||
       typeof x === "number" ||
       typeof x === "string") {
-    return x;
+    return true;
 
-  } else if (x.to_json != null) {
-    return x.to_json();
-
-  } else {
-    fail("Cannot convert to JSON: " + x);
-  }
-};
-
-export const from_json = (x) => {
-  if (x === null ||
-      x === true ||
-      x === false ||
-      typeof x === "number" ||
-      typeof x === "string") {
-    return x;
 
   } else if (Array["isArray"](x)) {
-    return List(map(x, from_json));
+    for (let i = 0; i < x["length"]; ++i) {
+      if (!is_json(x[i])) {
+        return false;
+      }
+    }
 
-  } else if (typeof x === "object") {
-    return Record(map(entries(x), ([key, value]) => [key, from_json(value)]));
+    return true;
+
+
+  } else if (is_object(x)) {
+    // TODO is this inefficient ?
+    // TODO what about hasOwnProperty check ?
+    for (let key in x) {
+      if (!is_json(x[key])) {
+        return false;
+      }
+    }
+
+    return true;
+
 
   } else {
-    fail("Cannot convert from JSON: " + x);
+    return false;
   }
 };
