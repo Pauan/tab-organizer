b8901ffb8e52e0dfa2e4082b86d21b48897e1799
Pauan <pcxunlimited@gmail.com>
2015-08-31 11:43:15 -1000

    Adding in new list.js and record.js files

---
+++ src/util/list.js
@@ -0,0 +1,133 @@
+import { Some, None } from "../maybe";
+import { assert, fail } from "../assert";
+
+
+const index_in_range = (index, len) =>
+  index >= 0 && index < len;
+
+const check_index = (index) => {
+  assert(typeof index === "number");
+};
+
+const get_index = (index, len) => {
+  check_index(index);
+
+  // TODO test this
+  if (index < 0) {
+    index += len;
+  }
+
+  if (index_in_range(index, len)) {
+    return index;
+
+  } else {
+    fail(new Error("Invalid index: " + index));
+  }
+};
+
+
+const _insert = (array, index, value) => {
+  // TODO test this
+  // TODO maybe have the check for "unshift" before the check for "push" ?
+  if (index === 0) {
+    array["unshift"](value);
+
+  } else if (index === length(array)) {
+    array["push"](value);
+
+  } else {
+    array["splice"](index, 0, value);
+  }
+};
+
+const _remove = (array, index) => {
+  // TODO test this
+  if (index === 0) {
+    array["shift"]();
+
+  // TODO test this
+  // TODO maybe have the check for "pop" before the check for "shift" ?
+  } else if (index === length(array) - 1) {
+    array["pop"]();
+
+  } else {
+    array["splice"](index, 1);
+  }
+};
+
+
+export const length = (array) =>
+  array["length"];
+
+export const clear = (array) => {
+  array["length"] = 0;
+};
+
+export const insert = (array, index, value) => {
+  check_index(index);
+};
+
+export const has = (array, index) => {
+  check_index(index);
+
+  const len = length(array);
+
+  // TODO test this
+  if (index < 0) {
+    index += len;
+  }
+
+  return index_in_range(index, len);
+};
+
+export const get = (array, i) => {
+  const index = get_index(i, length(array));
+  return array[index];
+};
+
+export const index_of = (array, value) => {
+  const index = array["indexOf"](value);
+
+  if (index === -1) {
+    return None;
+
+  } else {
+    return Some(index);
+  }
+};
+
+export const update = (array, i, new_value) => {
+  const index = get_index(i, length(array));
+
+  const old_value = array[index];
+
+  if (old_value !== new_value) {
+    array[index] = new_value;
+  }
+};
+
+export const insert = (array, i, value) => {
+  // TODO is this correct ?
+  const index = get_index(i, length(array) + 1);
+
+  _insert(array, index, value);
+};
+
+export const remove = (array, i) => {
+  const index = get_index(i, length(array));
+
+  _remove(array, index);
+};
+
+export const push = (array, value) => {
+  array["push"](value);
+};
+
+export const modify = (array, index, f) => {
+  const old_value = get(array, index);
+  const new_value = f(old_value);
+
+  if (old_value !== new_value) {
+    array[index] = new_value;
+  }
+};
---
+++ src/util/record.js
@@ -0,0 +1,78 @@
+import { each, entries, iterator } from "./iterator";
+import { to_json } from "./json";
+import { assert, fail } from "./assert";
+
+
+const check_key = (key) => {
+  assert(typeof key === "string" || typeof key === "number");
+};
+
+export const has = (obj, key) => {
+  check_key(key);
+  return key in obj;
+};
+
+export const get = (obj, key) => {
+  check_key(key);
+
+  if (key in obj) {
+    return obj[key];
+
+  } else {
+    fail(new Error("Key not found: " + key));
+  }
+};
+
+export const insert = (obj, key, value) => {
+  check_key(key);
+
+  if (key in obj) {
+    fail(new Error("Key already exists: " + key));
+
+  } else {
+    obj[key] = value;
+  }
+};
+
+export const modify = (obj, key, f) => {
+  const old_value = get(obj, key);
+  const new_value = f(old_value);
+
+  if (old_value !== new_value) {
+    obj[key] = new_value;
+  }
+};
+
+export const update = (obj, key, new_value) => {
+  const old_value = get(obj, key);
+
+  if (old_value !== new_value) {
+    obj[key] = new_value;
+  }
+};
+
+export const assign = (obj, key, value) => {
+  check_key(key);
+
+  obj[key] = value;
+};
+
+// TODO maybe change this to accept a thunk rather than a value ?
+export const default = (obj, key, value) => {
+  check_key(key);
+
+  if (!(key in obj)) {
+    obj[key] = value;
+  }
+};
+
+export const remove = (obj, key) => {
+  check_key(key);
+
+  if (key in obj) {
+    delete obj[key];
+
+  } else {
+    fail(new Error("Key not found: " + key));
+  }
+};
