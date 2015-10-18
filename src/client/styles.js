cd058d3729312edc1a7bff56c52f5f223fc2256b
Pauan <pcxunlimited@gmail.com>
2015-10-18 11:03:52 -1000

    Major improvements to the scrollbars

--- src/client/styles.js
+++ src/client/styles.js
@@ -57,29 +57,31 @@ dom.make_stylesheet("table", {
 
 
 // Styling for the scrollbar
-dom.make_stylesheet("::-webkit-scrollbar-button", {
-  "height": ref.always("0px"),
-  "display": ref.always("block"),
-  "background-color": ref.always("transparent")
+dom.make_stylesheet("::-webkit-scrollbar", {
+  "width": ref.always("12px"),
+  "height": ref.always("12px"),
+  "overflow": ref.always("visible")
 });
 
-dom.make_stylesheet("::-webkit-scrollbar", {
-  "width": ref.always("10px"),
-  "background": ref.always("-webkit-linear-gradient(left,rgba(202,202,202,0.07) 0%,rgba(229,229,229,0.07) 100%)"),
-  "box-shadow": ref.always("0 0 1px 0 rgba(0,0,0,.15) inset,0 1px 0 0 #fff"),
-  "background-color": ref.always("#e5e5e5"),
+dom.make_stylesheet("::-webkit-scrollbar-track", {
+  "border": ref.always("1px solid"),
+  "border-color": ref.always(dom.hsl(0, 0, 94)),
+  "background-color": ref.always(dom.hsl(0, 0, 96)),
+
+  //"margin-top": ref.always("1px"), // 3px
 
   "overflow": ref.always("visible"),
-  "border-radius": ref.always("4px"),
-  "border": ref.always("solid 1px #a6a6a6")
+  "border-radius": ref.always("5px")
 });
 
 dom.make_stylesheet("::-webkit-scrollbar-thumb", {
-  "background": ref.always("-webkit-linear-gradient(left,rgba(233,233,233,0.05) 0%,rgba(221,221,221,0.05) 100%)"),
-  "box-shadow": ref.always("0 2px 1px 0 rgba(0,0,0,.05)"),
-  "background-color": ref.always("#e9e9e9"),
+  "border": ref.always("2px solid"),
+  "border-color": ref.always(dom.hsl(0, 0, 97)),
+  "background-color": ref.always(dom.hsl(0, 0, 80)),
+
+  "box-shadow": ref.always("inset 0px 0px 0px 1px " + dom.hsl(0, 0, 60) + "," +
+                           "inset 0px 0px 0px 2px " + dom.hsl(0, 0, 85)),
 
   "overflow": ref.always("visible"),
-  "border-radius": ref.always("4px"),
-  "border": ref.always("solid 1px #a6a6a6")
+  "border-radius": ref.always("5px")
 });
