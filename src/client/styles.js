512ca3f818481e571890191e242296af65a342eb
Pauan <pcxunlimited@gmail.com>
2015-10-18 08:42:35 -1000

    Adding in custom scrollbars

--- src/client/styles.js
+++ src/client/styles.js
@@ -54,3 +54,32 @@ dom.make_stylesheet("input, textarea, keygen, select, button", {
 dom.make_stylesheet("table", {
   "border-spacing": ref.always("0px")
 });
+
+
+// Styling for the scrollbar
+dom.make_stylesheet("::-webkit-scrollbar-button", {
+  "height": ref.always("0px"),
+  "display": ref.always("block"),
+  "background-color": ref.always("transparent")
+});
+
+dom.make_stylesheet("::-webkit-scrollbar", {
+  "width": ref.always("10px"),
+  "background": ref.always("-webkit-linear-gradient(left,rgba(202,202,202,0.07) 0%,rgba(229,229,229,0.07) 100%)"),
+  "box-shadow": ref.always("0 0 1px 0 rgba(0,0,0,.15) inset,0 1px 0 0 #fff"),
+  "background-color": ref.always("#e5e5e5"),
+
+  "overflow": ref.always("visible"),
+  "border-radius": ref.always("4px"),
+  "border": ref.always("solid 1px #a6a6a6")
+});
+
+dom.make_stylesheet("::-webkit-scrollbar-thumb", {
+  "background": ref.always("-webkit-linear-gradient(left,rgba(233,233,233,0.05) 0%,rgba(221,221,221,0.05) 100%)"),
+  "box-shadow": ref.always("0 2px 1px 0 rgba(0,0,0,.05)"),
+  "background-color": ref.always("#e9e9e9"),
+
+  "overflow": ref.always("visible"),
+  "border-radius": ref.always("4px"),
+  "border": ref.always("solid 1px #a6a6a6")
+});
