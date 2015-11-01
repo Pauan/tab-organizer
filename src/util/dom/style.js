30dfaf20013360cf7bd0fedb08e2646e2483851e
Pauan <pcxunlimited@gmail.com>
2015-11-01 08:36:30 -1000

    Adding in prefixed values for CSS styles

--- src/util/dom/style.js
+++ src/util/dom/style.js
@@ -4,6 +4,38 @@ import * as ref from "../ref";
 import { assert, crash } from "../assert";
 
 
+const key_prefixes = {
+  // TODO it's a bit hacky to use the prefix system for this purpose...
+  //"width": ["width", "min-width", "max-width"],
+  //"height": ["height", "min-height", "max-height"],
+  "box-sizing": ["-moz-box-sizing", "box-sizing"], // TODO get rid of this later
+  "filter": ["-webkit-filter"] // TODO add in "filter" later
+};
+
+const value_prefixes = {
+  "grab": ["-webkit-grab", "grab"],
+  "grabbing": ["-webkit-grabbing", "grabbing"]
+};
+
+const prefix_key = (x) => {
+  if (key_prefixes[x]) {
+    return key_prefixes[x];
+  } else {
+    return [x];
+  }
+};
+
+const prefix_value = (x) => {
+  if (x === null) {
+    return [x];
+  } else if (value_prefixes[x]) {
+    return value_prefixes[x];
+  } else {
+    return [x];
+  }
+};
+
+
 export const has_style = (style, key) =>
   record.has(style, key);
 
@@ -29,42 +61,67 @@ const stringify = (x) => {
   }
 };
 
-export const set_style_value = (() => {
-  const prefixes = {
-    // TODO it's a bit hacky to use the prefix system for this purpose...
-    //"width": ["width", "min-width", "max-width"],
-    //"height": ["height", "min-height", "max-height"],
-    "box-sizing": ["-moz-box-sizing", "box-sizing"], // TODO get rid of this later
-    "filter": ["-webkit-filter"] // TODO add in "filter" later
-  };
 
-  return (style, key, value, important = false) => {
-    // TODO test this
-    if (typeof key !== "string") {
-      crash(new Error("Key must be a string: " + key));
-    }
+const check_style_value = (value) => {
+  // TODO test this
+  if (value !== null && typeof value !== "string") {
+    crash(new Error("Value must be null or a string: " + value));
+  }
 
-    // TODO test this
-    if (value !== null && typeof value !== "string") {
-      crash(new Error("Value must be null or a string: " + value));
-    }
+  if (value === "") {
+    crash(new Error("Value cannot be \"\", use `null` instead"));
+  }
+};
 
-    if (value === "") {
-      crash(new Error("Value cannot be \"\", use `null` instead"));
-    }
+const check_style_important = (important) => {
+  if (typeof important !== "boolean") {
+    crash(new Error("Important must be a boolean: " + important));
+  }
+};
 
-    const keys = (prefixes[key]
-                   ? prefixes[key]
-                   : [key]);
+const get_style_values = (x) => {
+  if (x === null || typeof x === "string") {
+    check_style_value(x);
+    return prefix_value(x);
 
-    let seen = false;
+  } else {
+    check_style_value(x.value);
+    return prefix_value(x.value);
+  }
+};
 
-    list.each(keys, (key) => {
-      if (has_style(style, key)) {
-        seen = true;
+const get_style_important = (x) => {
+  if (x === null || typeof x === "string") {
+    return "";
 
-        const old_value = get_style_value(style, key);
+  } else {
+    check_style_important(x.important);
+    return (x.important ? "important" : "");
+  }
+};
 
+
+export const set_style_value = (style, key, value) => {
+  // TODO test this
+  if (typeof key !== "string") {
+    crash(new Error("Key must be a string: " + key));
+  }
+
+  const keys = prefix_key(key);
+
+  const values = get_style_values(value);
+
+  const important = get_style_important(value);
+
+  let seen = false;
+
+  list.each(keys, (key) => {
+    if (has_style(style, key)) {
+      seen = true;
+
+      const old_value = get_style_value(style, key);
+
+      list.each(values, (value) => {
         if (old_value !== value) {
           // TODO test this
           if (value === null) {
@@ -73,7 +130,7 @@ export const set_style_value = (() => {
           } else {
             // TODO does this trigger a relayout ?
             // https://drafts.csswg.org/cssom/#dom-cssstyledeclaration-setproperty
-            style["setProperty"](key, value, (important ? "important" : ""));
+            style["setProperty"](key, value, important);
           }
 
 
@@ -83,14 +140,14 @@ export const set_style_value = (() => {
             crash(new Error("Invalid value (\"" + key + "\": " + stringify(value) + ")"));
           }
         }
-      }
-    });
-
-    if (!seen) {
-      crash(new Error("Invalid keys (\"" + list.join(keys, "\", \"") + "\")"));
+      });
     }
-  };
-})();
+  });
+
+  if (!seen) {
+    crash(new Error("Invalid keys (\"" + list.join(keys, "\", \"") + "\")"));
+  }
+};
 
 
 const e = document["createElement"]("style");
