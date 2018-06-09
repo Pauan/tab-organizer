d975d89781f12208b244035ed68aecfb12813ad4
Pauan <pcxunlimited@gmail.com>
2018-06-09 12:10:19 -1000

    More improvements

--- src/bin/sidebar/main.rs
+++ src/bin/sidebar/main.rs
@@ -9,10 +9,10 @@ extern crate stdweb;
 use std::rc::Rc;
 use dominator::traits::*;
 use dominator::text;
-use dominator::animation::{Percentage, MutableAnimation};
+use dominator::animation::{Percentage, MutableAnimation, AnimatedMapBroadcaster};
 use dominator::animation::easing;
 use dominator::events::MouseOverEvent;
-use futures_signals::signal::{Mutable, SignalExt};
+use futures_signals::signal::{Signal, Mutable, SignalExt};
 use futures_signals::signal_vec::{MutableVec, SignalVecExt};
 
 
@@ -39,7 +39,7 @@ impl Tab {
 
 
 struct Dragging {
-    tab: Option<usize>,
+    tab_index: Option<usize>,
 }
 
 struct State {
@@ -60,6 +60,9 @@ impl State {
 
                     // The index of the tab which has the id `id`
                     let mut new_index = None;
+
+                    let old_index = dragging.tab_index.unwrap_or_else(|| slice.len());
+                    let new_index = slice.iter().position(|tab| tab.id == id).unwrap();
 
                     for (index, tab) in slice.iter().enumerate() {
                         let old_done = if let None = old_index {
@@ -176,19 +179,20 @@ impl State {
 
 fn main() {
     let state = Rc::new(State {
-        tabs: MutableVec::new_with_values((0..5).map(|id| {
+        tabs: MutableVec::new_with_values((0..5000).map(|id| {
             Rc::new(Tab::new(id, "foo", "foo"))
         }).collect()),
         dragging: Mutable::new(None),
     });
 
-    let mut top_id = 5;
+    let mut top_id = 999999;
 
     js! { @(no_return)
         setInterval(@{clone!(state => move || {
+            state.tabs.remove(0);
             state.tabs.insert_cloned(0, Rc::new(Tab::new(top_id, "foo", "foo")));
             top_id += 1;
-        })}, 1000);
+        })}, 550);
     }
 
     let group_style = class! {
@@ -197,16 +201,25 @@ fn main() {
     };
 
     let tab_style = class! {
+        style("cursor", "pointer");
+        style("display", "flex");
         style("box-sizing", "border-box");
         style("position", "relative");
         style("background-color", "white");
-        style("border", "1px solid red");
-        style("margin", "-1px");
-        style("padding-left", "5px");
-        style("padding-right", "5px");
+        style("border", "1px solid transparent");
+        style("padding", "1px");
         style("overflow", "hidden");
-        style("height", "25px");
+        style("border-radius", "5px");
+        style("height", "20px");
+        //style("transform", "translate3d(0px, 0px, 0px)");
     };
+
+    let tab_text_style = class! {
+        style("padding-left", "3px");
+        style("padding-right", "1px");
+    };
+
+    log!("Starting");
 
     dominator::append_dom(&dominator::body(),
         html!("div", {
@@ -221,28 +234,49 @@ fn main() {
                 }
             }));
 
-            children_signal_vec(state.tabs.signal_vec_cloned().animated_map(300.0, move |tab, height| {
+            children_signal_vec(state.tabs.signal_vec_cloned().animated_map(500.0, move |tab, height| {
                 if state.should_be_dragging(tab.id) {
                     tab.drag_over.jump_to(Percentage::new(1.0));
+                }
+
+                fn px(t: Option<Percentage>, min: f64, max: f64) -> Option<String> {
+                    t.map(|t| format!("{}px", easing::in_out(t, easing::cubic).range_inclusive(min, max).round()))
+                }
+
+                fn height_signal(height: &AnimatedMapBroadcaster, min: f64, max: f64) -> impl Signal<Item = Option<String>> {
+                    height.signal().map(move |t| px(t.none_if(1.0), min, max))
                 }
 
                 html!("div", {
                     class(&tab_style);
 
-                    style_signal("margin-left", height.signal().map(|t| {
-                        t.none_if(1.0).map(|t| format!("{}px", easing::in_out(t, easing::cubic).range_inclusive(20.0, -1.0)))
+                    style_signal("margin-left", height_signal(&height, 12.0, 0.0));
+                    style_signal("height", height_signal(&height, 0.0, 20.0));
+                    style_signal("padding-top", height_signal(&height, 0.0, 1.0));
+                    style_signal("padding-bottom", height_signal(&height, 0.0, 1.0));
+                    style_signal("border-top-width", height_signal(&height, 0.0, 1.0));
+                    style_signal("border-bottom-width", height_signal(&height, 0.0, 1.0));
+
+                    style_signal("opacity", height.signal().map(|t| {
+                        t.none_if(1.0).map(|t| easing::in_out(t, easing::cubic).range_inclusive(0.0, 1.0).to_string())
                     }));
 
-                    style_signal("height", height.signal().map(|t| {
-                        t.none_if(1.0).map(|t| format!("{}px", easing::in_out(t, easing::cubic).range_inclusive(0.0, 25.0)))
-                    }));
+                    style_signal("top", tab.drag_over.signal().map(|t| px(t.none_if(0.0), 0.0, 20.0)));
 
-                    style_signal("top", tab.drag_over.signal().map(|t| {
-                        t.none_if(0.0).map(|t| format!("{}px", easing::in_out(t, easing::cubic).range_inclusive(0.0, 25.0)))
-                    }));
+                    //style_signal("transform", tab.drag_over.signal().map(|t| t.none_if(0.0).map(|t| format!("translate3d(0px, {}px, 0px)", easing::in_out(t, easing::cubic).range_inclusive(0.0, 20.0).round()))));
 
                     children(&mut [
-                        text(&tab.title)
+                        html!("div", {
+                            class(&tab_text_style);
+
+                            style_signal("transform", height.signal().map(|t| {
+                                t.none_if(1.0).map(|t| format!("rotateX({}deg)", easing::in_out(t, easing::cubic).range_inclusive(-90.0, 0.0)))
+                            }));
+
+                            children(&mut [
+                                text(&tab.title)
+                            ]);
+                        }),
                     ]);
 
                     event(clone!(state => move |_: MouseOverEvent| {
@@ -252,4 +286,6 @@ fn main() {
             }));
         }),
     );
+
+    log!("Finished");
 }
